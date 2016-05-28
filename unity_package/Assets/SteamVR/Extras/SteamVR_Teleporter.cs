f7a01256f8f167c33c7a2ee5f021ab799682ecc6
mdintaman <m.dintaman@gmail.com>
2016-05-28 00:40:54 -0500

    Update SteamVR_Teleporter.cs
    added 
    
    hasGroundTarget= true;
    
    to line 71.  There are probably better ways to determine if you want to proceed, but this is better than the alternative where it will never work.
    
    Altered the calculation for the new Camera Rig position on line 84.  Believe it should be contact world point of raycast offset by local point of hmd.  This should move the player to the targeted point as opposed to the current implementation where it appears to move Camera rig to targeted point offset by hmd and controller, which is disorienting.

--- unity_package/Assets/SteamVR/Extras/SteamVR_Teleporter.cs
+++ unity_package/Assets/SteamVR/Extras/SteamVR_Teleporter.cs
@@ -68,6 +68,9 @@ public class SteamVR_Teleporter : MonoBehaviour
                 RaycastHit hitInfo;
                 Physics.Raycast(ray, out hitInfo);
                 dist = hitInfo.distance;
+                
+                //Alter this line if you want to limit the collisions that will result in successful teleport.
+                hasGroundTarget = true;
             }
             else
             {
@@ -77,7 +80,8 @@ public class SteamVR_Teleporter : MonoBehaviour
             if (hasGroundTarget)
             {
 				Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
-				t.position = ray.origin + ray.direction * dist - new Vector3(t.GetChild(0).localPosition.x, 0f, t.GetChild(0).localPosition.z) - headPosOnGround;
+				
+				t.position = ray.origin + ray.direction * dist - headPosOnGround;
             }
         }
     }
